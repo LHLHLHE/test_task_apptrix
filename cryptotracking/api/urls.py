fc0e914153621043f220e7cf2681ca9afe504df0
Лев Халяпин <lev.halyapin@yandex.ru>
2023-03-17 01:10:48 +0300

    front

--- cryptotracking/api/urls.py
+++ cryptotracking/api/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 
-from api.views import CurrencyListAPIView
+from .views import CurrencyRetrieveAPIView, CurrencyListAPIView
 
 urlpatterns = [
     path('v1/auth/', include('djoser.urls.authtoken')),
     path(
         'v1/currencies/',
         CurrencyListAPIView.as_view(),
-        name='currencies_list'
+        name='currency_list'
+    ),
+    re_path(
+        r'v1/currencies/(?P<currency_symbol>[A-Z]+)',
+        CurrencyRetrieveAPIView.as_view(),
+        name='currency'
     ),
     path('v1/', include('djoser.urls.base')),
 ]
--- cryptotracking/api/views.py
+++ cryptotracking/api/views.py
@@ -4,42 +4,65 @@ import os
 from dotenv import load_dotenv
 from requests import Session, Timeout, TooManyRedirects
 from rest_framework import permissions
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 load_dotenv()
 
 API_KEY = os.getenv('COIN_MARKET_CAP_API_KEY')
-API_URL = 'https://pro-api.coinmarketcap.com/v1'
+API_URL = 'https://pro-api.coinmarketcap.com'
 
 HEADERS = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': f'{API_KEY}',
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': f'{API_KEY}',
 }
-
-
-def find_by_key(data, symbol):
-    for currency_data in data:
-        if currency_data['symbol'] == symbol:
-            return currency_data
 
 
 class CurrencyListAPIView(APIView):
     permission_classes = (permissions.AllowAny,)
+    renderer_classes = (TemplateHTMLRenderer,)
+    template_name = 'index.html'
 
     def get(self, request):
-        parameters = {'convert': 'USD'}
+        session = Session()
+        session.headers.update(HEADERS)
+
+        params = request.query_params
+
+        try:
+            response = session.get(
+                API_URL + '/v1/cryptocurrency/listings/latest',
+                params=params
+            )
+            data = json.loads(response.text).get('data')
+            return Response({'data': data, 'convert': params.get('convert')})
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            return Response({'error': e})
+
+
+class CurrencyRetrieveAPIView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, currency_symbol):
+        parameters = {
+            'symbol': currency_symbol
+        } | request.query_params
 
         session = Session()
         session.headers.update(HEADERS)
 
         try:
             response = session.get(
-                API_URL+'/cryptocurrency/listings/latest',
+                API_URL+'/v2/cryptocurrency/quotes/latest',
                 params=parameters
             )
-            data = find_by_key(json.loads(response.text)['data'], 'FET')
-            # data = json.loads(response.text)['data']
+            data = json.loads(response.text).get('data')
             return Response(data)
         except (ConnectionError, Timeout, TooManyRedirects) as e:
             return Response({'error': e})
+
+
+# class AddToFavoritesAPIView(APIView):
+#     def post(self, request, currency_symbol):
+
